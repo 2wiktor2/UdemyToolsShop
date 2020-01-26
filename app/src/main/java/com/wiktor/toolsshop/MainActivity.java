413ffa68ab2dd0f3abf1596c5823db234371ae2c
2wiktor2 <wiktor2011@gmail.com>
2020-01-26 23:58:13 +0300

    2

--- app/src/main/java/com/wiktor/toolsshop/MainActivity.java
+++ app/src/main/java/com/wiktor/toolsshop/MainActivity.java
@@ -1,13 +1,40 @@
 package com.wiktor.toolsshop;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listViewTools;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewTools = findViewById(R.id.listViewTools);
+        listViewTools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "Позиция : "+ i, Toast.LENGTH_SHORT).show();
+                switch (i){
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(), ActivityDrillCategory.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
     }
 }
